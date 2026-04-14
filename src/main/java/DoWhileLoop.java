c6d845f0a9f92dfb36a1042cf428bdf87ae39863
CodeHart-art <jobhart11211@gmail.com>
2026-04-14 11:43:21 -0400

    Step one complete

---
+++ src/main/java/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println("i love java");
+            i++;
+        }
+        while (i < 5);
+    }
+}
